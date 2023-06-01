c73d8f25d53cf4d1335b97a375b3ed17b592b21b
kelvin901 <wawerukelvin2019@gmail.com>
2023-06-01 15:26:58 +0300

    Completed Assignment

--- lib/student.rb
+++ lib/student.rb
@@ -1,9 +1,78 @@
 require_relative "../config/environment.rb"
 
+require 'sqlite3'
+
+DB = {:conn => SQLite3::Database.new("db/students.db")}
+
 class Student
+  attr_accessor :id, :name, :grade
 
-  # Remember, you can access your database connection anywhere in this class
-  #  with DB[:conn]
+  def initialize(id=nil, name, grade)
+    @id = id
+    @name = name
+    @grade = grade
+  end
 
+  def self.create_table
+    sql = <<-SQL
+      CREATE TABLE IF NOT EXISTS students (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        grade INTEGER
+      )
+    SQL
 
+    DB[:conn].execute(sql)
+  end
+
+  def self.drop_table
+    sql = "DROP TABLE IF EXISTS students"
+    DB[:conn].execute(sql)
+  end
+
+  def save
+    sql = "INSERT INTO students (name, grade) VALUES (?, ?)"
+    DB[:conn].execute(sql, self.name, self.grade)
+
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+  end
+
+  def self.create(name, grade)
+    student = Student.new(name, grade)
+    student.save
+    student
+  end
+
+  def self.new_from_db(row)
+    id = row[0]
+    name = row[1]
+    grade = row[2]
+    Student.new(id, name, grade)
+  end
+
+  def self.find_by_name(name)
+    sql = "SELECT * FROM students WHERE name = ? LIMIT 1"
+    DB[:conn].execute(sql, name).map do |row|
+      self.new_from_db(row)
+    end.first
+  end
+
+  def update
+    sql = "UPDATE students SET name = ?, grade = ? WHERE id = ?"
+    DB[:conn].execute(sql, self.name, self.grade, self.id)
+  end
 end
+
+# Create the students table
+Student.create_table
+
+# Create a new student and save it to the database
+student = Student.create("Kelvin Nyoike", 24)
+
+# Find a student by name
+found_student = Student.find_by_name("Kelvin Nyoike")
+puts found_student.name # Output: Kelvin Nyoike
+
+# Update the grade of a student
+student.grade = 13
+student.update
